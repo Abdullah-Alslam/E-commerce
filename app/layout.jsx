7fe9d279b3589f85f52415fcb4dc5bc1b2c99218
Abdullah <abdullahxyzabc67@gmail.com>
2025-09-14 11:31:44 +0300

    fix : edit rootlayout in layout page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,15 @@
+// app/layout.js
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressContentEditableWarning>
-      <body className="bg-white ">
-          <Header />
-          <main>{children}</main>
-          <Footer />
+      <body className="bg-white">
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
