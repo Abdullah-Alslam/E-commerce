1b1e868fdfadbf29ee868c7fc2e26de71edd3bc1
Abdullah <abdullahxyzabc67@gmail.com>
2025-09-16 15:31:18 +0300

    feat: create smart watch page using reusable ProductsList component

---
+++ app/products/watches/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import ProductsList from "../../components/ProductsList";
+
+
+export default function SmartWatchesPage() {
+  return (
+    <ProductsList
+      category="Smart Watches"
+      title="Our Latest Smart Watches"
+      product="smart watches"
+      link="https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?auto=format&fit=crop&w=800&q=80"
+    />
+  );
+}
