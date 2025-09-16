bbe8e7e99697f6f40cc12383e48971492144724f
Abdullah <abdullahxyzabc67@gmail.com>
2025-09-16 15:35:03 +0300

    feat: create Accessories page with updated hero image

---
+++ app/products/accessories/page.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+import ProductsList from "../../components/ProductsList";
+
+
+export default function AccessoriesPage() {
+  return (
+    <ProductsList
+      title="Our Accessories"
+      product="Accessories"
+      category="Accessories"
+      link="https://images.unsplash.com/photo-1593642532400-2682810df593?auto=format&fit=crop&w=800&q=80"
+    />
+  );
+}
