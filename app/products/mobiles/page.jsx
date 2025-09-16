6ebfb33b649fa64294c44a8625eb42840f2a8632
Abdullah <abdullahxyzabc67@gmail.com>
2025-09-16 15:18:57 +0300

    "Refactor mobiles page: simplify layout using reusable ProductsList component with props"

---
+++ app/products/mobiles/page.jsx
@@ -0,0 +1,16 @@
+// app/mobiles/page.js
+"use client";
+
+import ProductsList from "../../components/ProductsList";
+
+
+export default function MobilesPage() {
+  return (
+    <ProductsList
+      title="Our Latest Mobiles"
+      product="mobiles"
+      category="Mobiles"
+      link="https://images.unsplash.com/photo-1511707171634-5f897ff02aa9" // صورة تمثيلية للموبايلات
+    />
+  );
+}
