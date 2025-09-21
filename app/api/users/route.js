3d5756277415073f9c9b05a5f8970bfad7212da0
Abdullah <abdullahxyzabc67@gmail.com>
2025-09-21 11:09:20 +0300

    feat : create user API : delete ,  put ,get

---
+++ app/api/users/route.js
@@ -0,0 +1,58 @@
+// /app/api/users/route.js
+import { NextResponse } from "next/server";
+import connectToDatabase from "@/lib/mongodb";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  createdAt: Date,
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const users = await User.find({}, "-password").sort({ createdAt: -1 }); // بدون الباسورد
+    return NextResponse.json(users, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+    if (!id) return NextResponse.json({ error: "User ID required" }, { status: 400 });
+
+    await connectToDatabase();
+    await User.findByIdAndDelete(id);
+
+    return NextResponse.json({ message: "User deleted successfully" }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
+
+
+export async function PUT(req) {
+  try {
+    const { id, name, email } = await req.json();
+    if (!id || !name || !email)
+      return NextResponse.json({ error: "All fields are required" }, { status: 400 });
+
+    await connectToDatabase();
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      { name, email },
+      { new: true }
+    );
+
+    return NextResponse.json({ message: "User updated successfully", user: updatedUser }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
