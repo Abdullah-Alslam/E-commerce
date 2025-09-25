7fba26d330333796e569e8ebe4a9d3670f80fc78
Abdullah <abdullahxyzabc67@gmail.com>
2025-09-25 13:22:54 +0300

    chore : install react toastify library , and add it to layout file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,15 @@
 // app/layout.js
-"use client";
+"use client"; // مهم إذا استخدمنا usePathname أو Toastify
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { usePathname } from "next/navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
+
   const hideLayout =
     pathname === "/signup" ||
     pathname === "/login" ||
@@ -18,6 +21,20 @@ export default function RootLayout({ children }) {
       <body>
         {!hideLayout && <Header />}
         {children}
+
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+
         {!hideLayout && <Footer />}
       </body>
     </html>
