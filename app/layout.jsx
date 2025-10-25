17d39c2ebf397f6eaa73afa836094dcd15a820e3
Abdullah <abdullahxyzabc67@gmail.com>
2025-10-25 13:58:44 +0300

    feat : adding Roboto line to the E-Commerce

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
-// app/layout.jsx
-"use client"; // مهم إذا استخدمنا usePathname أو Toastify
+
+"use client"; 
 
 import "./globals.css";
 import Header from "./components/Header";
@@ -8,6 +8,15 @@ import { usePathname } from "next/navigation";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { ThemeProvider } from "next-themes";
+
+
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  display: "swap",
+});
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
@@ -20,7 +29,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
+      <body
+        className={`${roboto.className} bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300`}
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {!hideLayout && <Header />}
           <main>{children}</main>
@@ -42,3 +53,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+   
