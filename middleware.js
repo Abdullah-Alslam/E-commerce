5b83dd3461414687dda83db7c183cc48fbaee3c3
Abdullah <abdullahxyzabc67@gmail.com>
2025-11-19 23:46:36 +0300

    feat L create middleware file to protect route

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
+
+export async function middleware(req) {
+  const token = req.cookies.get("token")?.value;
+
+  if (!token) {
+    return NextResponse.rewrite(new URL("/401", req.url));
+  }
+
+  try {
+    await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
+    return NextResponse.next();
+  } catch (e) {
+    return NextResponse.rewrite(new URL("/401", req.url));
+  }
+}
+
+export const config = {
+  matcher: ["/account/:path*", "/wishlist", "/cart", "/contact"],
+};
